8f4a1405e85e4e75029575abb5a1ab20dbf7b348
saurav981 <souravhalder981@gmail.com>
2025-06-05 03:56:35 +0530

    changed PORT

--- Backend/server.js
+++ Backend/server.js
@@ -13,10 +13,13 @@ app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 // ROUTES
 app.use("/api/scholarships", scholarshipRoutes);
 
+// ping this every 10 minutes using UptimeRobot to keep server alive, and avoid cold start
+app.get("/health", (req, res) => res.sendStatus(200));
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("Error in MongoDB connection", err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`App running on port: ${PORT}`));
